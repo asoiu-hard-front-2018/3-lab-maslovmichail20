403251fdcebb42c5a899eec883f8129287f3626c
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-10-22 14:54:07 +0300

    Add change blog posts animation

--- src/js/sidebarChangeHandler.js
+++ src/js/sidebarChangeHandler.js
@@ -1,5 +1,11 @@
 function sidebarSetActiveHandler(element) {
     element.classList.add('sidebar__content_active');
+
+    const blog = [document.querySelector('.post-wrapper'), document.querySelector('.post_size-big')];
+    blog.forEach(elem => elem.classList.add('animate-blog'));
+    setTimeout(() => {
+        blog.forEach(elem => elem.classList.remove('animate-blog'));
+    }, 1000);
 }
 
 function resetPrevActive() {
